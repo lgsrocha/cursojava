df169b202e5d8d543aaa82e6e6c789316a30e4a5
lgsrocha <lucasgabriels2210@gmail.com>
2022-06-03 11:45:54 -0300

    Adiciona exercicio com os modificadores Static e Final

---
+++ ex5_11/MatematicaUtil.java
@@ -0,0 +1,16 @@
+//Exercicio com variaveis do tipo Static e Final
+package ex5_11;
+
+public class MatematicaUtil {
+	// http://pt.wikipedia.org/wiki/Número_de_Fibonacci
+	public static int calcularFibonacci(int posicao) {
+		if (posicao < 2) {
+			return posicao;
+		}
+		return calcularFibonacci(posicao - 1) + calcularFibonacci(posicao - 2);
+	}
+	public static final double PI = 3.14;
+	public static double calcularAreaCirculo(double raio){
+		return ((raio * raio) * PI);
+	}
+}
